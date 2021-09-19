d0ebe68ffb85dffcdfa7cf2037d50c57484294fa
unknown <nayefkanaan03@gmail.com>
2021-09-20 02:44:54 +0300

    Add github username

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ const inquirer = require('inquirer');
 
 
 
-const questions = [];
+const questions = [
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+      validate: userInput => {
+        if (userInput) {
+            return true;
+        } else {
+            console.log('Please enter your user name.');
+            return false; 
+        }
+    } 
+    },
+    {
+      type: '',
+      message: '',
+      name: '',
+    },
+    {
+      type: '',
+      message: '',
+      name: '',
+    },
+    {
+      type: '',
+      message: '',
+      name: '',
+    },
+    {
+      type: '',
+      message: '',
+      name: '', 
+    },
+  ])
+];
 
 
 function writeToFile(fileName, data) {}
